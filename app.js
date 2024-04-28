371044cef55733627d9d4ca5898e89ee29de0ff3
LAVEEZAFAREED <162338181+LAVEEZAFAREED@users.noreply.github.com>
2024-04-28 04:03:26 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+export {};
+//DAY 8 CODING CHALLENGE:
+//Question 1:Intentional Error:Try to produce an array index error in one of your program.Correct the error before finis
---
+++ app.ts
@@ -0,0 +1,2 @@
+//DAY 8 CODING CHALLENGE:
+//Question 1:Intentional Error:Try to produce an array index error in one of your program.Correct the error before finis
